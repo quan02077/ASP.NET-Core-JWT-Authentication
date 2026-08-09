9a6641d6636f25801fc9f0d61a5f7959ed21e8a8
Quan Nguyen <quan02077@gmail.com>
2026-08-09 16:25:07 +0700

    Tạo JWT token

--- JWTAuthDotNET/JWTAuthDotNET/Controllers/AuthController.cs
+++ JWTAuthDotNET/JWTAuthDotNET/Controllers/AuthController.cs
@@ -3,12 +3,17 @@ using JWTAuthDotNET.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace JWTAuthDotNET.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class AuthController : ControllerBase
+    public class AuthController(IConfiguration configuration) : ControllerBase
     {
         public static User user = new User();
 
@@ -19,6 +24,44 @@ namespace JWTAuthDotNET.Controllers
             user.Username = request.Username;
             user.PasswordHash = hashedPassword;
             return Ok(user);
+        }
+        [HttpPost("login")]
+        public ActionResult<string> Login(UserDTO request)
+        {
+            if(user.Username != request.Username)
+            {
+                return BadRequest("User not found");
+            }
+            if(new PasswordHasher<User>().VerifyHashedPassword(user, user.PasswordHash, request.Password) == PasswordVerificationResult.Failed)
+            {
+                return BadRequest("Wrong password");
+            }
+            string token = CreateToken(user);
+            return Ok(token);
+        }
+
+        public string CreateToken(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Username)
+            };
+            var tokenKey = configuration["AppSettings:Token"]
+                ?? throw new Exception("Chưa cấu hình AppSettings:Token trong appsettings.json!");
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512);
+
+            var tokenDescriptor = new JwtSecurityToken(
+                issuer: configuration.GetValue<string>("AppSettings: Issuer"),
+                audience: configuration.GetValue<string>("AppSettings: Audience"),
+                claims: claims,
+                expires: DateTime.UtcNow.AddDays(1),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(tokenDescriptor);
         }
     }
 }
