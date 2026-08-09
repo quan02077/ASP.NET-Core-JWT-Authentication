91c931e408074b52e2559089567f39c20925164f
Quan Nguyen <quan02077@gmail.com>
2026-08-09 15:35:11 +0700

    Tạo Register với băm mật khẩu

---
+++ JWTAuthDotNET/JWTAuthDotNET/Controllers/AuthController.cs
@@ -0,0 +1,24 @@
+﻿using JWTAuthDotNET.Entities;
+using JWTAuthDotNET.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTAuthDotNET.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public static User user = new User();
+
+        [HttpPost("register")]
+        public ActionResult<User> Register(UserDTO request)
+        {
+            var hashedPassword = new PasswordHasher<User>().HashPassword(user, request.Password);
+            user.Username = request.Username;
+            user.PasswordHash = hashedPassword;
+            return Ok(user);
+        }
+    }
+}
---
+++ JWTAuthDotNET/JWTAuthDotNET/Models/UserDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace JWTAuthDotNET.Models
+{
+    public class UserDTO
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
