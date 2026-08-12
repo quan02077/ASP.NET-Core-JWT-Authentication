877d149ac14ea82805a093315208981addb704a5
Quan Nguyen <quan02077@gmail.com>
2026-08-12 14:29:39 +0700

    Tao context

---
+++ JWTAuthDotNET/JWTAuthDotNET/Data/UserDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace JWTAuthDotNET.Data
+{
+    public class UserDbContext: DbContext
+    {
+        public UserDbContext(DbContextOptions<UserDbContext> options) : base(options) { }
+        public DbSet<Entities.User> Users { get; set; } = null!;
+    }
+}
