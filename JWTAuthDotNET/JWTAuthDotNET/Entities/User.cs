0174cdb3856a9126e02ab7b3283c5857e7a1da1d
Quan Nguyen <quan02077@gmail.com>
2026-08-14 15:12:13 +0700

    Sửa cho datetime có thể null từ đầu

--- JWTAuthDotNET/JWTAuthDotNET/Entities/User.cs
+++ JWTAuthDotNET/JWTAuthDotNET/Entities/User.cs
@@ -7,6 +7,6 @@
         public string PasswordHash { get; set; } = string.Empty;
         public string Role { get; set; } = string.Empty;
         public string? RefreshToken { get; set; }
-        public DateTime RefreshTokenExpiryTime { get; set; }
+        public DateTime? RefreshTokenExpiryTime { get; set; }
     }
 }
